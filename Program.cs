9f5041c17c2a0aa5c469d9704944400f21dc9bf6
Omar <Omar@DESKTOP-OUKQKJG>
2025-02-12 19:42:10 +0300

    video 2

--- Program.cs
+++ Program.cs
@@ -42,6 +42,35 @@ namespace LinQ2
             //    Console.WriteLine(item);
             //} 
             #endregion
+
+
+            #region video2
+            //var result = ProductList.Select(p => p);
+            //var result = ProductList.Select(p => p.ProductName);
+
+            //var result = from p in ProductList
+            //             select p.ProductName;
+
+
+            //var result = ProductList.Where(p => p.UnitsInStock > 0 && p.Category == "Seafood")
+            //    .Select(p => new {p.Category , oldprice =  p.ProductName , newprice = p.UnitPrice - p.UnitPrice *0.1m});
+
+
+            //var result = CustomerList.Select(c => c.CustomerName);
+            //var result = CustomerList.SelectMany(c => c.Orders);
+
+
+            //var result = from c in CustomerList
+            //             form o in c.orders
+            //             select o;
+
+            // var result = ProductList.Select((p, i) => new { i, p.ProductName }).Where (p=>p.i<5);
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //} 
+            #endregion
+
         }
     }
 }
